c95365cc67da24b33e0d0e502e6312b79e9c2e66
Yaraslaw <yaroslav.akhramenko@gmail.com>
2025-12-12 17:35:30 +0100

    init

--- src/backend/config/config_utils.py
+++ src/backend/config/config_utils.py
@@ -1,5 +1,5 @@
 import yaml
 
 def read_config(name: str):
-    with open(f"src/backend/config/{name}.yaml", "r") as f:
+    with open(f"/Users/yaroslav/Downloads/SE1-Research-Showcase-Portal-main/src/backend/config/{name}.yaml", "r") as f:
         return yaml.safe_load(f)
